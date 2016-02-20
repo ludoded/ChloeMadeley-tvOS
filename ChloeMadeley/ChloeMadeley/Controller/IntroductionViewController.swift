0f50bedb9baf5ea5a71b584cf800d6b6bb727736
Haik <haik.ampardjian@gmail.com>
2016-02-20 20:39:58 +0200

    [Add] Video files

--- ChloeMadeley/ChloeMadeley/Controller/IntroductionViewController.swift
+++ ChloeMadeley/ChloeMadeley/Controller/IntroductionViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class IntroductionViewController: UIViewController {
-    @IBOutlet weak var videoLayer: UIView!
+    @IBOutlet weak var videoLayer: VideoView!
     @IBOutlet weak var descriptionTextView: UITextView!
+    
+    var avPlayer: AVPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         descriptionTextView.selectable = true
         descriptionTextView.panGestureRecognizer.allowedTouchTypes = [NSNumber(integer: UITouchType.Indirect.rawValue)]
+        
+        let videoPath = NSBundle.mainBundle().pathForResource("intro", ofType: "mov") ?? ""
+        let videoURL = NSURL(fileURLWithPath: videoPath)
+        avPlayer = AVPlayer(URL: videoURL)
+        
+        let playerLayer = AVPlayerLayer(player: avPlayer)
+        playerLayer.frame = videoLayer.bounds
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        playerLayer.needsDisplayOnBoundsChange = true
+        videoLayer.layer.addSublayer(playerLayer)
+        videoLayer.layer.needsDisplayOnBoundsChange = true
+        avPlayer?.play()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: "playerItemDidReachEnd:",
+            name: AVPlayerItemDidPlayToEndTimeNotification,
+            object: avPlayer!.currentItem)
+    }
+    
+    func playerItemDidReachEnd(notification: NSNotification) {
+        avPlayer?.seekToTime(kCMTimeZero)
+        avPlayer?.play()
     }
 }
